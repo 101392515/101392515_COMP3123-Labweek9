40778854151756f84bb1addbdfce11c162103eff
101392515 <zain.karedia@georgebrown.ca>
2023-11-12 21:51:45 -0500

    Week9

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
+  const studentName = 'Zain Karedia';
+  const studentId = '101392515';
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to Fullstack Development - I</h1>
+        <p>React JS Programming Week09 Lab exercise</p>
+        <p>Your Name: {studentName}</p>
+        <p>Your Student ID: {studentId}</p>
+        <p>George Brown College, Toronto</p>
       </header>
     </div>
   );
